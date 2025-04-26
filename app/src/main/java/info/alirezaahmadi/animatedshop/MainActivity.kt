f910c8317d3536e955b93e0f803f306f79ba3cc4
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-26 22:55:25 +0330

    add nav in MainActivity

--- app/src/main/java/info/alirezaahmadi/animatedshop/MainActivity.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/MainActivity.kt
@@ -9,8 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import info.alirezaahmadi.animatedshop.navigation.NavGraph
 import info.alirezaahmadi.animatedshop.ui.theme.AnimatedShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +23,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AnimatedShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                CompositionLocalProvider( LocalLayoutDirection provides LayoutDirection.Rtl) {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        NavGraph(
+                            modifier = Modifier.fillMaxSize()
+                                .padding(innerPadding)
+                        )
+                    }
                 }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimatedShopTheme {
-        Greeting("Android")
     }
 }
