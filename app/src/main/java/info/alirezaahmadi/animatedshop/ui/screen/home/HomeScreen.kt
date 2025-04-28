92b74364c0c140ef5ac033a158fcb69478d238e0
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-28 20:50:53 +0330

    call flow in HomeScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/HomeScreen.kt
@@ -12,8 +12,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -33,11 +36,12 @@ fun HomeScreen(
     animatedContentScope: AnimatedContentScope,
     mainViewModel: MainViewModel
 ) {
+   val allCategory by mainViewModel.allCategory.collectAsState()
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
         item { SliderSection() }
-        item { HomeCategorySection()  }
+        item { HomeCategorySection(categoryList = allCategory)  }
         item {
             with(sharedTransitionScope) {
                 Row(
