faa0b49444394495c3e9ff243577d11f9ab8704c
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-28 17:50:14 +0330

    create ProductDetailScreen

---
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/detail/ProductDetailScreen.kt
@@ -0,0 +1,57 @@
+package info.alirezaahmadi.animatedshop.ui.screen.detail
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun ProductDetailScreen(
+    navHostController: NavHostController,
+    image:Int,
+    name:String,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope
+) {
+    with(sharedTransitionScope) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(image),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .sharedElement(
+                        sharedContentState = sharedTransitionScope.rememberSharedContentState(key = "image-$image"),
+                        animatedVisibilityScope = animatedContentScope,
+                    )
+
+                    .aspectRatio(1f)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = name, fontSize = 18.sp,
+                modifier =
+                Modifier
+                    .sharedElement(
+                        sharedTransitionScope.rememberSharedContentState(key = "text-${name}"),
+                        animatedVisibilityScope = animatedContentScope
+                    )
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
