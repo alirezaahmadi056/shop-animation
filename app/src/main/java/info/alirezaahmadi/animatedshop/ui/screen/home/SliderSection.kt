1c823b2cfcdae0d79aa12509ab7b773b35f5b618
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-28 17:00:31 +0330

    update SliderSection

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/SliderSection.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/SliderSection.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,11 +19,6 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
-import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowRight
-import androidx.compose.material.icons.rounded.KeyboardArrowRight
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,13 +26,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import info.alirezaahmadi.animatedshop.R
 
@@ -112,7 +105,7 @@ private fun SliderItem(
             }
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.padding(start = 15.dp)
+                modifier = Modifier.padding(horizontal = 12.dp)
             ) {
                 Text(
                     text = "از ورزشت لذت ببر!",
@@ -120,10 +113,12 @@ private fun SliderItem(
                     color = Color.White
                 )
                 Text(
-                    text = "تولید و عرضه انواع کفش های اسپرت در همه سایز ها",
+                    text = "تولید و عرضه انواع کفش های\n اسپرت در همه سایز ها",
                     style = MaterialTheme.typography.bodyMedium,
                     textAlign = TextAlign.Center,
-                    color = Color.White
+                    color = Color.White,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
