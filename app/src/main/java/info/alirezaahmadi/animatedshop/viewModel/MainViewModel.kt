b781d738716a77273bc3fa01f9f860e15df0f39d
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-29 21:35:15 +0330

    add  function shopping in viewModel

--- app/src/main/java/info/alirezaahmadi/animatedshop/viewModel/MainViewModel.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/viewModel/MainViewModel.kt
@@ -3,9 +3,11 @@ package info.alirezaahmadi.animatedshop.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import info.alirezaahmadi.animatedshop.data.db.entity.ShoppingEntity
 import info.alirezaahmadi.animatedshop.data.model.Category
 import info.alirezaahmadi.animatedshop.repository.MainRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,11 +17,41 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: MainRepository
-):ViewModel() {
+) : ViewModel() {
 
-    private val _allCategory:MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
-    val allCategory :StateFlow<List<Category>> =_allCategory.asStateFlow()
+    private val _allCategory: MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
+    val allCategory: StateFlow<List<Category>> = _allCategory.asStateFlow()
 
-    init { viewModelScope.launch(Dispatchers.IO) { _allCategory.emit(repository.getAllCategory()) } }
+    init {
+        viewModelScope.launch(Dispatchers.IO) { _allCategory.emit(repository.getAllCategory()) }
+    }
+    //shopping
+    fun upsertShoppingItem(shoppingEntity: ShoppingEntity) {
+        viewModelScope.launch(Dispatchers.IO) { repository.upsertShoppingItem(shoppingEntity) }
+    }
+
+    fun getSingleShoppingItem(itemId: Int): Flow<ShoppingEntity?> =
+        repository.getSingleShoppingItem(itemId)
+
+    fun getAllShoppingItems(): Flow<List<ShoppingEntity>> =
+        repository.getAllShoppingItems()
+
+    fun deletedShoppingItem(shoppingEntity: ShoppingEntity) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deletedShoppingItem(shoppingEntity)
+        }
+    }
+
+    fun updateShoppingItemCount(
+        itemId: Int,
+        newCount: Int,
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.updateShoppingItemCount(itemId = itemId, newCount = newCount)
+        }
+    }
+
+    fun isHaveItemToCart(itemId: Int): Flow<Boolean> = repository.isHaveItemToCart(itemId)
+
 
 }
