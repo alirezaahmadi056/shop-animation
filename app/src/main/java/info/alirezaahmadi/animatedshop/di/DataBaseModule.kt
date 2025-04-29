6683cf96065c78410eab54b76fef2c49c985652c
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-29 21:32:35 +0330

    create dataBaseModule

---
+++ app/src/main/java/info/alirezaahmadi/animatedshop/di/DataBaseModule.kt
@@ -0,0 +1,34 @@
+package info.alirezaahmadi.animatedshop.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import info.alirezaahmadi.animatedshop.data.db.AppDatBase
+import info.alirezaahmadi.animatedshop.data.db.dao.ShoppingDao
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DataBaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDataBase(
+       @ApplicationContext context: Context
+    ) :AppDatBase{
+        return Room.databaseBuilder(
+            context = context,
+            name = "my_dp",
+            klass = AppDatBase::class.java
+        ).build()
+    }
+    @Provides
+    @Singleton
+    fun provideShoppingDao(appDatBase: AppDatBase):ShoppingDao{
+        return appDatBase.ShoppingDao()
+    }
+}
