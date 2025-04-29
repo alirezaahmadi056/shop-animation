5878e3b444840238d4c51d95239ca60d775eeb1a
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-29 21:34:29 +0330

    create ShoppingDao functions

---
+++ app/src/main/java/info/alirezaahmadi/animatedshop/data/db/dao/ShoppingDao.kt
@@ -0,0 +1,32 @@
+package info.alirezaahmadi.animatedshop.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import info.alirezaahmadi.animatedshop.data.db.entity.ShoppingEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ShoppingDao {
+    @Upsert
+    suspend fun upsertShoppingItem(shoppingEntity: ShoppingEntity)
+
+    @Query("select *  from shoppingentity where id=:itemId ")
+    fun getSingleShoppingItem(itemId: Int): Flow<ShoppingEntity?>
+
+    @Query("select * from shoppingentity")
+    fun getAllShoppingItems(): Flow<List<ShoppingEntity>>
+
+    @Delete
+    suspend fun deletedShoppingItem(shoppingEntity: ShoppingEntity)
+
+    @Query("update ShoppingEntity set count=:newCount where id=:itemId")
+    suspend fun updateCount(
+        itemId: Int,
+        newCount: Int,
+    )
+    @Query("select exists(select * from ShoppingEntity where id=:itemId) ")
+    fun isHasIsCart(itemId: Int): Flow<Boolean>
+
+}
