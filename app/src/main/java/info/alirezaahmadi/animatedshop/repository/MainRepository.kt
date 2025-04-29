2c6c4619c39e99fc6b37bc1753bda7374e8c1de9
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-29 21:35:05 +0330

    add  function shopping in repo

--- app/src/main/java/info/alirezaahmadi/animatedshop/repository/MainRepository.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/repository/MainRepository.kt
@@ -1,11 +1,41 @@
 package info.alirezaahmadi.animatedshop.repository
 
+import info.alirezaahmadi.animatedshop.data.db.dao.ShoppingDao
+import info.alirezaahmadi.animatedshop.data.db.entity.ShoppingEntity
 import info.alirezaahmadi.animatedshop.data.model.Category
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class MainRepository @Inject constructor(){
+class MainRepository @Inject constructor(
+    private val shoppingDao: ShoppingDao
+) {
 
-     fun getAllCategory() :List<Category>{
+    fun getAllCategory(): List<Category> {
         return Category.fakeCategories
     }
+
+    //shopping
+    suspend fun upsertShoppingItem(shoppingEntity: ShoppingEntity) {
+        shoppingDao.upsertShoppingItem(shoppingEntity)
+    }
+
+    fun getSingleShoppingItem(itemId: Int): Flow<ShoppingEntity?> =
+        shoppingDao.getSingleShoppingItem(itemId)
+
+    fun getAllShoppingItems(): Flow<List<ShoppingEntity>> =
+        shoppingDao.getAllShoppingItems()
+
+    suspend fun deletedShoppingItem(shoppingEntity: ShoppingEntity) {
+        shoppingDao.deletedShoppingItem(shoppingEntity)
+    }
+
+    suspend fun updateShoppingItemCount(
+        itemId: Int,
+        newCount: Int,
+    ) {
+        shoppingDao.updateCount(itemId = itemId, newCount = newCount)
+    }
+
+    fun isHaveItemToCart(itemId: Int): Flow<Boolean> = shoppingDao.isHasIsCart(itemId)
+
 }
