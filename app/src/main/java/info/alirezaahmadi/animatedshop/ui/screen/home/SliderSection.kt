cc38b689dd9a0edca7a253b6be0269378b638f60
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-29 10:44:00 +0330

    add scroll in SliderSection

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/SliderSection.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/SliderSection.kt
@@ -2,12 +2,16 @@ package info.alirezaahmadi.animatedshop.ui.screen.home
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,10 +23,16 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,14 +40,17 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import info.alirezaahmadi.animatedshop.R
+import kotlinx.coroutines.launch
 
 @Composable
 fun SliderSection() {
-    val pagerState = rememberPagerState { 5 }
+    val pagerState = rememberPagerState(initialPage = 2) { 5 }
+    val scope= rememberCoroutineScope()
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -47,13 +60,17 @@ fun SliderSection() {
         HorizontalPager(
             modifier = Modifier.fillMaxSize(),
             state = pagerState,
+            contentPadding = PaddingValues(horizontal = 5.dp)
         ) { page ->
             SliderItem(
                 tabSize = pagerState.pageCount,
-                page = page
+                page = page,
+                scrollBack = {
+                    scope.launch { pagerState.animateScrollToPage(page =page-1, animationSpec = tween(650)) } },
+                scrollNext = { scope.launch { pagerState.animateScrollToPage(page =page+1, animationSpec = tween(650)) } }
             )
         }
-        Row (
+        Row(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .padding(bottom = 15.dp),
@@ -72,7 +89,15 @@ fun SliderSection() {
 private fun SliderItem(
     tabSize: Int,
     page: Int,
+    scrollNext: () -> Unit,
+    scrollBack: () -> Unit
 ) {
+    val enableNext = remember(key1 = tabSize, key2 = page) { tabSize > page+1 }
+    val enableBack = remember(key1 = page) { page > 0 }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+    ) {
         Row(
             modifier = Modifier
                 .padding(8.dp)
@@ -109,20 +134,61 @@ private fun SliderItem(
             ) {
                 Text(
                     text = "از ورزشت لذت ببر!",
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = Color.White
+                    style = MaterialTheme.typography.titleLarge,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
                 )
+                Spacer(Modifier.height(6.dp))
                 Text(
                     text = "تولید و عرضه انواع کفش های\n اسپرت در همه سایز ها",
                     style = MaterialTheme.typography.bodyMedium,
                     textAlign = TextAlign.Center,
                     color = Color.White,
                     maxLines = 2,
+                    fontWeight = FontWeight.SemiBold,
                     overflow = TextOverflow.Ellipsis
                 )
             }
         }
+        if (enableBack) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color.White)
+                    .clickable(onClick = scrollBack),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowRight,
+                    contentDescription = "",
+                    modifier = Modifier.padding(vertical = 12.dp),
+                    tint = Color.Black
+                )
+            }
+        }
+        if (enableNext) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color.White)
+                    .clickable(onClick = scrollNext),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowLeft,
+                    contentDescription = "",
+                    modifier = Modifier.padding(vertical = 12.dp),
+                    tint = Color.Black
+                )
+            }
+        }
+
+
     }
+
+}
 
 
 @Composable
