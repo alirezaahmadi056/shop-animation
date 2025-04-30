e4387d1cdc46f49b4e1c685a93e5a0dfa88729bd
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-30 09:45:34 +0330

    create faveEntity

---
+++ app/src/main/java/info/alirezaahmadi/animatedshop/data/db/entity/FavoriteEntity.kt
@@ -0,0 +1,16 @@
+package info.alirezaahmadi.animatedshop.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class FavoriteEntity(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Long,
+    val discountPercent: Int,
+    val image: Int,
+    val rating: Float,
+)
