f83a5e7887a2ec674157b2da60f48feaf0ea3fad
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-30 09:46:22 +0330

    update AppDatBase

--- app/src/main/java/info/alirezaahmadi/animatedshop/data/db/AppDatBase.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/data/db/AppDatBase.kt
@@ -2,11 +2,14 @@ package info.alirezaahmadi.animatedshop.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import info.alirezaahmadi.animatedshop.data.db.dao.FavoriteDao
 import info.alirezaahmadi.animatedshop.data.db.dao.ShoppingDao
+import info.alirezaahmadi.animatedshop.data.db.entity.FavoriteEntity
 import info.alirezaahmadi.animatedshop.data.db.entity.ShoppingEntity
-@TypeConverters(MyTypeConverters::class)
-@Database(entities = [ShoppingEntity::class], version = 1)
+
+@Database(entities = [ShoppingEntity::class,FavoriteEntity::class], version = 1)
 abstract class AppDatBase:RoomDatabase() {
+
     abstract fun ShoppingDao() :ShoppingDao
+    abstract fun FavoriteDao():FavoriteDao
 }
