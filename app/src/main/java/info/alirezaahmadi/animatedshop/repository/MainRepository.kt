5041e7dc8a01792c6176de881e5aec45179cba6e
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-30 13:55:08 +0330

    add functions fave in viewmodel

--- app/src/main/java/info/alirezaahmadi/animatedshop/repository/MainRepository.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/repository/MainRepository.kt
@@ -1,13 +1,19 @@
 package info.alirezaahmadi.animatedshop.repository
 
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import info.alirezaahmadi.animatedshop.data.db.dao.FavoriteDao
 import info.alirezaahmadi.animatedshop.data.db.dao.ShoppingDao
+import info.alirezaahmadi.animatedshop.data.db.entity.FavoriteEntity
 import info.alirezaahmadi.animatedshop.data.db.entity.ShoppingEntity
 import info.alirezaahmadi.animatedshop.data.model.Category
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
-    private val shoppingDao: ShoppingDao
+    private val shoppingDao: ShoppingDao,
+    private val favoriteDao: FavoriteDao
 ) {
 
     fun getAllCategory(): List<Category> {
@@ -38,4 +44,18 @@ class MainRepository @Inject constructor(
 
     fun isHaveItemToCart(itemId: Int): Flow<Boolean> = shoppingDao.isHasIsCart(itemId)
 
+
+    //favorite
+
+    suspend fun upsertFavoriteItem(favoriteEntity: FavoriteEntity){
+        favoriteDao.upsertFavoriteItem(favoriteEntity)
+    }
+
+    suspend fun deletedFavoriteItem(itemId: Int){
+        favoriteDao.deletedFavoriteItem(itemId)
+    }
+
+    fun isFavorite(itemId: Int): Flow<Boolean> =favoriteDao.isFavorite(itemId)
+
+    fun getAllFavorite(): Flow<List<FavoriteEntity>> =favoriteDao.getAllFavorite()
 }
