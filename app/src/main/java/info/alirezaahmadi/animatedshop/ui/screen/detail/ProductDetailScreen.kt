da55caa6f1527d59fe89c6971333697ca9842361
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-30 13:55:46 +0330

    update ProductDetailScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/detail/ProductDetailScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/detail/ProductDetailScreen.kt
@@ -5,19 +5,23 @@ import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -25,6 +29,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import info.alirezaahmadi.animatedshop.data.model.Product
+import info.alirezaahmadi.animatedshop.util.byLocate
 import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
 
 @OptIn(ExperimentalSharedTransitionApi::class)
@@ -62,19 +67,40 @@ fun ProductDetailScreen(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Spacer(Modifier.height(15.dp))
-                Image(
-                    painter = painterResource(image),
-                    contentDescription = "",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .sharedElement(
-                            sharedContentState = sharedTransitionScope.rememberSharedContentState(
-                                key = "image-${image}"
-                            ),
-                            animatedVisibilityScope = animatedContentScope,
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(top = 15.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    Image(
+                        painter = painterResource(image),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .sharedElement(
+                                sharedContentState = sharedTransitionScope.rememberSharedContentState(
+                                    key = "image-${image}"
+                                ),
+                                animatedVisibilityScope = animatedContentScope,
+                            )
+                            .fillMaxWidth(0.8f)
+                    )
+                    Box(
+                        modifier = Modifier.align(Alignment.TopStart)
+                            .clip(RoundedCornerShape(topEnd = 12.dp, bottomEnd = 12.dp))
+                            .background(Brush.linearGradient(listOf(Color(0xffFD583D),Color(0xffE32A0D),))),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = "$discountPercent%".byLocate(),
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            modifier = Modifier.padding(horizontal = 15.dp, vertical = 6.dp)
                         )
-                        .fillMaxWidth(0.8f)
-                )
+                    }
+                }
+
                 ProductDetailHeaderSection(
                     modifier = Modifier.align(Alignment.End),
                     product = singleProduct,
@@ -86,7 +112,7 @@ fun ProductDetailScreen(
                         .background(Color.White)
                         .padding(horizontal = 12.dp)
                 ) {
-                    Spacer(Modifier.height(12.dp))
+                    Spacer(Modifier.height(25.dp))
                     Text(
                         text =title,
                         style = MaterialTheme.typography.titleLarge,
