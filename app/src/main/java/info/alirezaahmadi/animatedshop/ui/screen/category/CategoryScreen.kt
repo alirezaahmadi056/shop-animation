56d1b714faf0db634f0c2fc02c8921d9f69fafe3
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-02 20:34:06 +0330

    update CategoryScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/category/CategoryScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/category/CategoryScreen.kt
@@ -3,9 +3,33 @@ package info.alirezaahmadi.animatedshop.ui.screen.category
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import info.alirezaahmadi.animatedshop.ui.component.ProductItemCard
 import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
 
 @OptIn(ExperimentalSharedTransitionApi::class)
@@ -16,5 +40,92 @@ fun CategoryScreen(
     animatedContentScope: AnimatedContentScope,
     mainViewModel: MainViewModel
 ) {
+    val allCategory by mainViewModel.allCategory.collectAsState()
+    val pagerState = rememberPagerState(3) { allCategory.size }
+    Scaffold(
+        topBar = {
+            CategoryTabsSection(
+                pagerState = pagerState,
+                categoryList = allCategory
+            )
+        },
+        containerColor = Color(0xffFCF3EC),
+    ) { innerPadding ->
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = innerPadding,
+            userScrollEnabled = false
+        ) { page ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                item {
+                    Text(
+                        text = allCategory[page].title,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)
+                    )
+                }
+                item {
+                    LazyRow(modifier = Modifier.fillMaxWidth()) {
+                        items(allCategory[page].products.reversed()) {
+                            ProductItemCard(
+                                product = it,
+                                navHostController = navHostController,
+                                sharedTransitionScope = sharedTransitionScope,
+                                animatedContentScope = animatedContentScope
+                            )
+                        }
+                    }
+                }
+                item { BestSellerProductTopSection()}
+                item {
+                    LazyRow(modifier = Modifier.fillMaxWidth()) {
+                        items(allCategory[page].products) {
+                            ProductItemCard(
+                                product = it,
+                                navHostController = navHostController,
+                                sharedTransitionScope = sharedTransitionScope,
+                                animatedContentScope = animatedContentScope
+                            )
+                        }
+                    }
+                }
 
+            }
+        }
+    }
+}
+
+@Composable
+private fun BestSellerProductTopSection() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "پرفروش ترین های هفته گذشته",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        TextButton(onClick = {}) {
+            Text(
+                text = "مشاهده همه",
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+            )
+            Icon(
+                imageVector = Icons.Rounded.ArrowBack,
+                contentDescription = "",
+                tint = Color.Black
+            )
+        }
+    }
 }
