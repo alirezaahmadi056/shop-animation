c7677fcc055e5402fca49eeaf71f1613258c7ffe
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-02 16:05:53 +0330

    create AppTopBar

---
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/component/AppTopBar.kt
@@ -0,0 +1,113 @@
+package info.alirezaahmadi.animatedshop.ui.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import info.alirezaahmadi.animatedshop.R
+import info.alirezaahmadi.animatedshop.navigation.Routes
+
+@Composable
+fun AppTopBar(
+    navHostController: NavHostController
+) {
+    val backStackEntry = navHostController.currentBackStackEntryAsState()
+    val currentGraph = backStackEntry.value?.destination?.route?.substringAfterLast(".")
+
+    val navigationRoute = listOf(
+        Routes.HomeScreen,
+        Routes.CategoryScreen,
+        Routes.ShoppingCartScreen,
+        Routes.ProfileScreen,
+    )
+    val showBack = currentGraph !in navigationRoute.map { it.toString() }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Rounded.Menu,
+                    contentDescription = "",
+                    tint = Color.Black
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.notification),
+                    contentDescription = "",
+                    tint = Color(0xffEF472C),
+                    modifier = Modifier.size(23.dp)
+                )
+
+
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.online_shop),
+                contentDescription = "",
+                modifier = Modifier.height(21.dp),
+                contentScale = ContentScale.Crop
+            )
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "",
+                modifier = Modifier.size(50.dp),
+                contentScale = ContentScale.Fit
+            )
+            AnimatedVisibility(
+                visible = showBack,
+                enter = expandHorizontally(tween(500)),
+                exit = shrinkHorizontally(tween(500)),
+            ) {
+            IconButton(onClick = { navHostController.navigateUp() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Rounded.ArrowForward,
+                    contentDescription = "",
+                    tint = Color.Black
+                )
+            }
+        }
+        }
+
+    }
+}
