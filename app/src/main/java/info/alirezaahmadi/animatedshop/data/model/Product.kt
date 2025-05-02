8e23c33395520171dee7b13a15ea0f36fdb35e58
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-02 11:33:43 +0330

    remove des

--- app/src/main/java/info/alirezaahmadi/animatedshop/data/model/Product.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/data/model/Product.kt
@@ -6,14 +6,13 @@ import info.alirezaahmadi.animatedshop.data.db.entity.FavoriteEntity
 data class Product(
     val id: Int,
     val title: String,
-    val description: String,
     val price: Long,
     val discountPercent: Int,
     val image: Int,
     val rating: Float,
 ){
     fun convertToFavoriteItem():FavoriteEntity{
-        return  FavoriteEntity(id, title, description, price, discountPercent, image, rating)
+        return  FavoriteEntity(id, title,price, discountPercent, image, rating)
     }
 }
 
@@ -33,7 +32,6 @@ data class Category(
                     Product(
                         id = 11,
                         title = "پیراهن دخترانه صورتی",
-                        description = "پیراهن زیبا و راحت مناسب مجالس.",
                         price = 450_000L,
                         discountPercent = 10,
                         image = R.drawable.img_shirt,
@@ -42,7 +40,6 @@ data class Category(
                     Product(
                         id = 12,
                         title = "تی‌شرت دخترانه",
-                        description = "تی‌شرت خنک تابستانی.",
                         price = 220_000L,
                         discountPercent = 5,
                         image = R.drawable.img_shirt6,
@@ -58,7 +55,6 @@ data class Category(
                     Product(
                         id = 21,
                         title = "ست سویشرت و شلوار مردانه",
-                        description = "سویشرت راحت مناسب پاییز و زمستان.",
                         price = 1_550_000L,
                         discountPercent = 10,
                         image = R.drawable.img_shirt1,
@@ -74,7 +70,6 @@ data class Category(
                     Product(
                         id = 31,
                         title = "پیراهن مجلسی زنانه",
-                        description = "پیراهن شیک و مجلسی مناسب مهمانی‌ها.",
                         price = 1_200_000L,
                         discountPercent = 15,
                         image = R.drawable.img_shirt8,
@@ -90,7 +85,6 @@ data class Category(
                     Product(
                         id = 41,
                         title = "کفش اسپرت مردانه",
-                        description = "کفش مناسب برای استفاده روزمره و ورزش.",
                         price = 980_000L,
                         discountPercent = 20,
                         image = R.drawable.img_shirt7,
@@ -99,7 +93,6 @@ data class Category(
                     Product(
                         id = 42,
                         title = "کفش دخترانه رنگی",
-                        description = "کفش راحتی مناسب کودکان.",
                         price = 370_000L,
                         discountPercent = 5,
                         image = R.drawable.img_shirt7,
@@ -115,7 +108,6 @@ data class Category(
                     Product(
                         id = 51,
                         title = "لباس نوزادی طرح خرس",
-                        description = "لباس راحت و نرم برای نوزادان.",
                         price = 250_000L,
                         discountPercent = 8,
                         image = R.drawable.img_shirt5,
@@ -131,7 +123,6 @@ data class Category(
                     Product(
                         id = 61,
                         title = "تی‌شرت پسرانه اسپرت",
-                        description = "تی‌شرت خنک و اسپرت برای پسران.",
                         price = 300_000L,
                         discountPercent = 12,
                         image = R.drawable.img_shirt10,
