eb691b4a239a5727c1813e63ff00e5f84889810b
Hosein_R <ihajhoseinam@gmail.com>
2025-05-03 12:35:06 +0330

    update ShoppingCartScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/shoping/ShoppingCartScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/shoping/ShoppingCartScreen.kt
@@ -1,12 +1,32 @@
 package info.alirezaahmadi.animatedshop.ui.screen.shoping
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
 
 @Composable
 fun ShoppingCartScreen(
-    navHostController: NavHostController
+    navHostController: NavHostController,
+    mainViewModel: MainViewModel,
 ) {
+
+    val products by mainViewModel.getAllShoppingItems().collectAsState(emptyList())
+
+    Scaffold(
+        bottomBar = {}
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = innerPadding
+        ) {
+
+        }
+    }
 
 }
