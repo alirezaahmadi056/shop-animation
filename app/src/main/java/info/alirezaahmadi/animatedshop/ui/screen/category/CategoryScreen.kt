1f7e08765937ee557370823d569fcd9d821a0078
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-08 12:53:43 +0330

    update CategoryScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/category/CategoryScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/category/CategoryScreen.kt
@@ -23,12 +23,14 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import info.alirezaahmadi.animatedshop.ui.component.BestSellerProductTopSection
 import info.alirezaahmadi.animatedshop.ui.component.ProductItemCard
 import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
 
@@ -36,7 +38,7 @@ import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
 @Composable
 fun CategoryScreen(
     navHostController: NavHostController,
-    selectedTabIndex:Int,
+    selectedTabIndex: Int,
     sharedTransitionScope: SharedTransitionScope,
     animatedContentScope: AnimatedContentScope,
     mainViewModel: MainViewModel
@@ -51,17 +53,29 @@ fun CategoryScreen(
             )
         },
         containerColor = Color(0xffFCF3EC),
+        modifier = Modifier.fillMaxSize()
     ) { innerPadding ->
         HorizontalPager(
             state = pagerState,
             modifier = Modifier.fillMaxSize(),
             contentPadding = innerPadding,
+            key = { "page$it" },
             userScrollEnabled = false
         ) { page ->
+            val product = remember(
+                key1 = page,
+                key2 = allCategory
+            ) { allCategory[page].products }
+
+                 val bestSellerProduct = remember(
+                key1 = page,
+                key2 = allCategory
+            ) { allCategory[page].bestProduct }
+
             LazyColumn(
                 modifier = Modifier.fillMaxSize(),
             ) {
-                item {
+                item(key = "title") {
                     Text(
                         text = allCategory[page].title,
                         style = MaterialTheme.typography.titleLarge,
@@ -70,9 +84,9 @@ fun CategoryScreen(
                         modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp)
                     )
                 }
-                item {
+                item(key = "product") {
                     LazyRow(modifier = Modifier.fillMaxWidth()) {
-                        items(allCategory[page].products.reversed(), key = {it.id}) {
+                        items(product, key = { it.id }) {
                             ProductItemCard(
                                 product = it,
                                 navHostController = navHostController,
@@ -82,38 +96,21 @@ fun CategoryScreen(
                         }
                     }
                 }
-                item { BestSellerProductTopSection()}
+                item(key = "BestSellerProductTopSection") { BestSellerProductTopSection() }
+                item(key = "bestSellerProduct") {
+                    LazyRow(modifier = Modifier.fillMaxWidth()) {
+                        items(bestSellerProduct, key = { it.id }) {
+                            ProductItemCard(
+                                product = it,
+                                navHostController = navHostController,
+                                sharedTransitionScope = sharedTransitionScope,
+                                animatedContentScope = animatedContentScope
+                            )
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-@Composable
-private fun BestSellerProductTopSection() {
-    Row(
-        modifier = Modifier.fillMaxWidth()
-            .padding(horizontal = 8.dp, vertical = 12.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = "پرفروش ترین های هفته گذشته",
-            style = MaterialTheme.typography.titleMedium,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black
-        )
-        TextButton(onClick = {}) {
-            Text(
-                text = "مشاهده همه",
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.SemiBold,
-                color = Color.Black
-            )
-            Icon(
-                imageVector = Icons.Rounded.ArrowBack,
-                contentDescription = "",
-                tint = Color.Black
-            )
-        }
-    }
-}
