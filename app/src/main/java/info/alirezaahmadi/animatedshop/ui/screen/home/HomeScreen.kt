450061ac9d43fd15089196a9eb8ac08446fc2bc3
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-08 12:47:28 +0330

    update HomeScreen

--- app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/ui/screen/home/HomeScreen.kt
@@ -3,30 +3,22 @@ package info.alirezaahmadi.animatedshop.ui.screen.home
 import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import info.alirezaahmadi.animatedshop.R
+import info.alirezaahmadi.animatedshop.data.model.Product
 import info.alirezaahmadi.animatedshop.navigation.Routes
 import info.alirezaahmadi.animatedshop.ui.component.ProductItemCard
 import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
@@ -40,8 +32,11 @@ fun HomeScreen(
     mainViewModel: MainViewModel
 ) {
     val allCategory by mainViewModel.allCategory.collectAsState()
+    val homeProduct = remember { Product.homeProduct }
+    val lazyListState = rememberLazyListState()
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        state = lazyListState
     ) {
         item { SliderSection() }
         item { Spacer(Modifier.height(15.dp)) }
@@ -51,8 +46,8 @@ fun HomeScreen(
             }
         }
         item {
-            LazyRow {
-                items(allCategory[0].products) {
+            LazyRow(modifier = Modifier.fillMaxWidth()) {
+                items(items = homeProduct, key = { it.id }) {
                     ProductItemCard(
                         product = it,
                         navHostController = navHostController,
