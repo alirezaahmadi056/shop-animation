c3bd79e78a1ee8620184b9d3c750f287386e04a6
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-05-12 23:33:54 +0330

    update MainActivity

--- app/src/main/java/info/alirezaahmadi/animatedshop/MainActivity.kt
+++ app/src/main/java/info/alirezaahmadi/animatedshop/MainActivity.kt
@@ -2,16 +2,17 @@ package info.alirezaahmadi.animatedshop
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -26,6 +27,7 @@ import info.alirezaahmadi.animatedshop.ui.component.DrawerContent
 import info.alirezaahmadi.animatedshop.ui.component.MainDrawer
 import info.alirezaahmadi.animatedshop.ui.theme.AnimatedShopTheme
 import info.alirezaahmadi.animatedshop.viewModel.MainViewModel
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -38,12 +40,16 @@ class MainActivity : ComponentActivity() {
             navHostController = rememberNavController()
             AnimatedShopTheme {
                 CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-                    val drawerState = rememberDrawerState(DrawerValue.Open)
+                    val drawerState = rememberDrawerState(DrawerValue.Closed)
+                    val coroutineScope = rememberCoroutineScope()
+                    val openDrawer ={ coroutineScope.launch { drawerState.open() } }
+                    val closeDrawer ={ coroutineScope.launch { drawerState.close() } }
+                    BackHandler(enabled = drawerState.isOpen) { closeDrawer.invoke() }
                     MainDrawer(
                         drawerState = drawerState,
-                        drawerContent ={
+                        drawerContent = {
                             DrawerContent(
-                                closeDrawer = {},
+                                closeDrawer = {closeDrawer.invoke()},
                                 navHostController = navHostController,
                                 mainViewModel = mainViewModel
                             )
@@ -54,7 +60,13 @@ class MainActivity : ComponentActivity() {
                                 .fillMaxSize(),
                             containerColor = Color(0xffFCF3EC),
                             bottomBar = { BottomNavigation(navHostController = navHostController) },
-                            topBar = {AppTopBar(navHostController=navHostController, mainViewModel = mainViewModel)}
+                            topBar = {
+                                AppTopBar(
+                                    navHostController = navHostController,
+                                    mainViewModel = mainViewModel,
+                                    openDrawer = { openDrawer.invoke()}
+                                )
+                            }
                         ) { innerPadding ->
                             NavGraph(
                                 mainViewModel = mainViewModel,
